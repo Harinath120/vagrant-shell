3010b75cd5958281a7de8da0bcc29af996032126
Harinath120 <konepalli.009@gmail.com>
2021-02-22 17:39:50 +0530

    read shell script

---
+++ scripts/read.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Enter your skills:"
+read SKILLS 
+
+echo "your $SKILLS skills are very important in IT Industry"
+
+read -p 'Username ' USR 
+read -sp 'Password: ' pass 
+
+echo
+
+echo "Login Successfull: Welcome USER $USR,"
